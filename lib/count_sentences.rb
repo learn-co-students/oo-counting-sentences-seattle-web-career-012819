e10a1835486af5e1250d54657f790eeb937c24d7
Lucy Suddenly <lucysuddenly@Lucys-Air.local.tld>
2019-01-30 08:04:49 -0800

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    words1 = self.split(".")
+    words2 = words1.map do |element|
+      element.split("?")
+    end
+    words3 = words2.flatten.map do
+      |element| element.split("!")
+    end
+    binding.pry
+    words3.flatten.reject(&:empty?).count
   end
 end
